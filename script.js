7a22f29146811072fce8b33d0575cfbd562aa978
alfauzian <aldifauzian611@gmail.com>
2022-05-01 02:30:20 +0700

    Course Final Exercise
    -adding some html element
    -add styling
    -create some function

--- script.js
+++ script.js
@@ -2,5 +2,33 @@ function compute()
 {
     p = document.getElementById("principal").value;
     
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value
+
+    var interest = principal * years * rate / 100;
+
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+    "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+    "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+    "in the year <mark>" + yearInTheFuture + "</mark>";
+
+
+
 }
-        
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+function validation() {
+    var principal = document.getElementById("principal").value;
+    var isPositiveNumber = parseInt(principal) > 0;
+    if(!isPositiveNumber) {
+        alert("Enter Positive Number");
+        document.getElementById("principal").focus();
+    }
+}  
